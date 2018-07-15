cbee09d32f83a437eec2ceb86533dce287f8836b
xn-- -i66bm0p6ma <zhuhongtao@silview.partner.onmschina.cn>
2018-07-15 07:25:02 +0000

    增加读取文件err处理

--- index.js
+++ index.js
@@ -15,12 +15,15 @@ console.log("Server running at http://localhost:%d", port);
 
 var fs = require('fs');
 
-var data = fs.readFileSync('input.txt');
-console.log(data.toString());
-console.log("程序执行结束");
+// var data = fs.readFileSync('input.txt');
+// console.log(data.toString());
+// console.log("程序执行结束");
 
 fs.readFile('input.txt',function(err,data){
-    if(err) return console.error(err);
+    if(err){
+        console.log(err.stack);
+        return;
+    } 
     console.log(data.toString());
 });
 console.log("程序执行结束");
